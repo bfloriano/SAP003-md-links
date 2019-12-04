b3cd5a3214a48714c4954c1d3bb10d3276661cfe
bfloriano <b.floriano@outlook.com>
2019-12-04 00:18:52 -0300

    readme pronto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
     "env": {
+        "browser": true,
         "es6": true,
         "node": true,
         "jest": true
     },
-    //"extends": "eslint:recommended",
+    "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -14,5 +15,15 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "camelcase": ["error", { "properties": "always" }],
+      "keyword-spacing": ["error", { "before": true }],
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "space-before-blocks": ["error", "always"],
+      "key-spacing": ["error", { "afterColon": true }],
+      "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
+      "no-multiple-empty-lines": ["error", { "max": 1 }],
     }
 };
--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 const mdLinks = require("./lib/index.js");
-const path = process.argv[2]
+const path = process.argv[2];
 
 mdLinks(path)
-    .then((resolveValue) => {
-        resolveValue.forEach(item => {console.log(`Title: ${item.text} URL: ${item.href}`);
-        });
-    })
-    .catch(console.error); 
+  .then((resolveValue) => {
+    resolveValue.forEach(item => {console.log(`Title: ${item.text} \nURL: ${item.href}\n`);
+    });
+  })
+  .catch(console.error); 
