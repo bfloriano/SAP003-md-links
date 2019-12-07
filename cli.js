015df984a8675c9e40d2238e29a2ba2bb7ebb83a
bfloriano <b.floriano@outlook.com>
2019-12-07 14:58:16 -0300

    correção gitignore hacker edition fail

--- cli.js
+++ cli.js
@@ -1,10 +1,17 @@
 #! /usr/bin/env node
 const mdLinks = require("./lib/index.js");
 const path = process.argv[2];
+const option = process.argv[3];
+// console.log(process.argv);
 
-mdLinks(path)
+mdLinks(path, option)
   .then((resolveValue) => {
-    resolveValue.forEach(item => {console.log(`\nTitle: ${item.text} \nURL: ${item.href}`);
-    });
+    if (option === "--validate") {
+      resolveValue.forEach(item => {console.log(`\nTitle: ${item.text} \nURL: ${item.href} \nStatus: ${item.statusText}`);
+      });
+    } else {
+      resolveValue.forEach(item => {console.log(`\nTitle: ${item.text} \nURL: ${item.href}`);
+      });
+    }
   })
   .catch(console.error); 
