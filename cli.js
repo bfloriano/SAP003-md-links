ec9857d46bae28c4b48733344ada673505be05dc
bfloriano <b.floriano@outlook.com>
2020-02-03 12:26:35 -0300

    hacker edition ok

--- cli.js
+++ cli.js
@@ -1,8 +1,6 @@
-#! /usr/bin/env node
 const mdLinks = require("./lib/index.js");
 const path = process.argv[2];
 const option = process.argv[3];
-// console.log(process.argv);
 
 mdLinks(path, option)
   .then((resolveValue) => {
